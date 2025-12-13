c7da00ece27d51558b7a480bf2b0fc0d0c7f1181
vanrey17 <elvanreynaldi434@gmail.com>
2025-12-13 22:32:05 +0700

    retry comment

--- page/dpib.php
+++ page/dpib.php
@@ -5,7 +5,7 @@ $page_description = "DPIB mempersiapkan siswa menjadi juru gambar arsitektur, dr
 
 ?>
 
-<!-- <main class="program-detail-section">
+<main class="program-detail-section">
     <div class="hero-background">
         <div class="text-with-gradient-bg">
             <h1><?php echo $page_title; ?></h1>
@@ -37,7 +37,7 @@ $page_description = "DPIB mempersiapkan siswa menjadi juru gambar arsitektur, dr
             <img src="<?php echo $image_path; ?>/labdpib.png" alt="Siswa DPIB Sedang Mengerjakan Desain Model 3D Bangunan">
         </div>
         
-    </div>   -->
+    </div>  
 
     <div class="kurikulum-section">
         <div class="container">
